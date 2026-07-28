1be46ef22537d43021c1a8ae72c50b68c1704874
Vadla Seetharam Achary <vadlasetharamachari@gmail.com>
2026-07-28 14:21:26 +0530

    Add files via upload

---
+++ codespace/Methodex1.java
@@ -0,0 +1,11 @@
+class MethodEx{
+ void Display(String name,String rollno){
+ System.out.println(name);
+ System.out.println(rollno);
+ }
+ public static void main(String args[]){
+   MethodEx m1=new MethodEx();
+   m1.Display("Siddhu","G4");
+   m1.Display("Ram","J3");
+   }
+  } 
